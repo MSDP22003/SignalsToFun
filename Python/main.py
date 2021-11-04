31c314e1be18f33032776b819e473463c54d3c44
KatieLeon <kel3796@rit.edu>
2021-11-03 22:43:46 -0400

    added set up for second plot

--- Python/main.py
+++ Python/main.py
@@ -5,46 +5,65 @@ import serial
 
 # initialize serial port
 ser = serial.Serial('COM4', 9600)
-
 ser.timeout = 10  # specify timeout
 ser.close()
 ser.open()
-emgsignal = ser.readline()  #declare variable for emg signal and have it read the serial port
+ser.readline()
 
 # this is printing serial parameters
 if ser.is_open:
     print("\nSerial port now open. Configuration:\n")
     print(ser, "\n")
 
-#style.use('fivethirtyeight') #makes graph more aesthetic but rn it's making things worse
+# style.use('ggplot') #might make graph more aesthetic but rn it's making things worse
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1)  #means the subplot is on a grid system that's 1x1
-ys = []  # defining the place to store y values
+# fig = plt.figure()
+fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+# ax1 = fig.add_subplot(1, 1, 1)  #means the subplot is on a grid system that's 1x1 #is no longer used for multiplots
+emgsignal1 = []  # defining the place to store y values
 
 
-def animate(i):  #i is the interval
+def animate(i):  # i is the interval
+
     if ser.inWaiting() > 0:
-        emgsignal = ser.read(ser.inWaiting()).decode('ascii')  # read the bytes and convert from binary array to ASCII
-        vals = emgsignal.split("\r\n")  #splits this by everywhere that theres a carriage return/new line
+        emgsignals = ser.read(ser.inWaiting()).decode('ascii')  # read the bytes and convert from binary array to ASCII
+        vals = emgsignals.split("\r\n")  # splits this by everywhere that theres a carriage return/new line
+
+        stringcount = len(vals)
+        count = int(stringcount)
+        print(count)
+
         newys = []
 
-        for v in vals:  #for loop for every value in the array called vals
-            if v != '':  #making sure no lines that are empty are being processed
-                newys.append(float(v.strip('\n\r')))  #getting rid of the new line & carriage return, removing any stragglers, & converting the serial input to a float
-        print(ys)
-        ys.extend(newys)
-        xs = [k for k in range(len(ys))]
-        ax1.clear()  #clears any data that was previously on the graph
-        ax1.set_xlim([len(ys)-len(newys)*10, len(ys)+100])
-        ax1.set_ylim([0, 1025])
+        for v in vals:  # for loop for every value in the array called vals
+            if v != '':  # making sure no lines that are empty are being processed
+                newys.append(float(v.strip('\n\r')))  # getting rid of the new line & carriage return, removing any stragglers, & converting the serial input to a float
+        emgsignal1.extend(newys)
+        xs = [k for k in range(len(emgsignal1))]
 
+        # replace emgsignal2 with the second EMG signal when available
+        # note: might need to add in an "x2" variable if for any reason it's not the same as xs
+        emgsignal2 = emgsignal1
+
+        # graph formatting for graph 1
+        ax1.clear()  # clears any data that was previously on the graph
+        ax1.set_xlim([len(emgsignal1) - len(newys) * 10, len(emgsignal1) + 100])
+        ax1.set_ylim([0, 25])
+        # graph formatting for graph 2
+        ax2.clear()  # clears any data that was previously on the graph
+        ax2.set_xlim([len(emgsignal1) - len(newys) * 10, len(emgsignal1) + 100])
+        ax2.set_ylim([0, 25])
+        # graph labeling
         plt.xlabel('Units')
         plt.ylabel("Signal (currently just noise)")
+        fig.suptitle('Adding a Second Subplot\n11/3/21')  # \n starts a new line
 
-        plt.title('Early Testing of Live Serial Animation Graph\n11/1/21')  # \n starts a new line
-        ax1.plot(xs, ys)
+        # plots the data
+        ax1.plot(xs, emgsignal1)
+        ax2.plot(xs, emgsignal2)
 
 
-ani = FuncAnimation(fig, animate, interval=50)  #updates the graph called fig with the function called animate every 1000 ms
+# update the graph called fig with the function called animate every 1000 ms
+ani = FuncAnimation(fig, animate, interval=50)
+
 plt.show()
--- Python/stackingsubplots.py
+++ Python/stackingsubplots.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
+x = [1, 2, 3]
+y = [4, 6, 9]
 
 fig, (ax1, ax2) = plt.subplots(2, sharex=True)
 fig.suptitle('Aligning x-axis using sharex')
-axs[0].plot(x, y ** 2)
-axs[1].plot(x, 0.3 * y, 'o')
-axs[2].plot(x, y, '+')
+
+ax1.plot(x, y)
+ax2.plot(x, y)
+
+plt.show()
