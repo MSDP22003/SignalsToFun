ac867f67d84dcb9c33cdabdac7f1106fee328ba4
KatieLeon <kel3796@rit.edu>
2021-11-15 23:57:08 -0500

    might be much simpler

--- EMGAcq/EMGAcq.ino
+++ EMGAcq/EMGAcq.ino
@@ -1,3 +1,24 @@
+// it might be as simple as:
+
+// Arduino pin numbers
+const int EMG1 = 0; // analog pin connected to EMG1 output
+const int EMG2 = 1; // analog pin connected to EMG2 output
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("EMG1: ");
+  Serial.print(analogRead(EMG1));
+  Serial.print("\n");
+  Serial.print("EMG2: ");
+  Serial.println(analogRead(EMG2));
+  Serial.print("\n\n");
+  delay(1000);
+}
+
+/* if not, try something more like this:
 const byte numChars = 32;
 char receivedChars[numChars];   // an array to store the received data
 
@@ -73,3 +94,4 @@ void showParsedData()
    Serial.println(frequency);
    newData = false;
 }
+*/
